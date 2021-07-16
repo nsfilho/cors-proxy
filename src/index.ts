e3b0887f1a1cfad6016c248502f3c3609a0f30d8
Nelio Santos <nsfilho@icloud.com>
2021-07-16 13:34:34 -0300

    feat(headers): remove headers from response

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
+/* eslint-disable no-console */
 import corsProxy from 'cors-anywhere';
 
 // Listen on a specific host via the HOST environment variable
 const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+const removeHeaders = ['cookie', 'cookie2', 'x-frame-options'];
+const removeHeadersFromResponse = ['x-frame-options', 'content-security-policy', 'access-control-expose-headers'];
 
 corsProxy
     .createServer({
         originWhitelist: [], // Allow all origins
         requireHeader: [],
-        removeHeaders: ['cookie', 'cookie2', 'x-frame-options'],
+        removeHeaders,
+        removeHeadersFromResponse,
     })
     .listen(port, host, () => {
-        // eslint-disable-next-line no-console
         console.log(`Running CORS Anywhere on ${host}:${port}`);
+        console.log('Remove from request:', removeHeaders);
+        console.log('Remove from response:', removeHeadersFromResponse);
     });
